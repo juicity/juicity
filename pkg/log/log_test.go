7e0873a4dba03c7fc12334d7520bd5d482ddb593
kev <yqlbu@bu.edu>
2023-07-31 21:45:01 +0800

    fixture(mock): add unit test

--- pkg/log/log_test.go
+++ pkg/log/log_test.go
@@ -5,13 +5,54 @@ import (
 	"time"
 )
 
+type testCase struct {
+	condition string
+	logger    *Logger
+}
+
 func TestLogger(t *testing.T) {
-	logger := NewLogger(&Options{
-		TimeFormat: time.DateTime,
-		LogFile:    "../../app.log",
-	})
+	// construct test cases
+	cases := []testCase{
+		{
+			condition: "ConsoleWriter (Alone)",
+			logger: NewLogger(&Options{
+				TimeFormat: time.DateTime,
+			}),
+		},
+		{
+			condition: "JsonWriter (Alone)",
+			logger: NewLogger(&Options{
+				TimeFormat:    time.DateTime,
+				JsonLogFormat: true,
+			}),
+		},
+		{
+			condition: "JsonWriter (Alone)",
+			logger: NewLogger(&Options{
+				TimeFormat:    time.DateTime,
+				JsonLogFormat: true,
+			}),
+		},
+		{
+			condition: "ConsoleWriter + FileWriter (in Stdout format)",
+			logger: NewLogger(&Options{
+				TimeFormat: time.DateTime,
+				LogFile:    "../../app_file_writer_stdout.log",
+			}),
+		},
+		{
+			condition: "JsonWrtier + FileWriter (in JSON format)",
+			logger: NewLogger(&Options{
+				TimeFormat:    time.DateTime,
+				LogFile:       "../../app_file_writer_json.log",
+				JsonLogFormat: true,
+			}),
+		},
+	}
 
-	const msg = "hello!"
-
-	logger.Info().Msg(msg)
+	for _, tc := range cases {
+		t.Run(tc.condition, func(t *testing.T) {
+			tc.logger.Info().Msg("Hello World!")
+		})
+	}
 }
