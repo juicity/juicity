11c8472b27ade7f0d0b2a0d611c30612d098893f
mzz <2017@duck.com>
2023-08-19 19:56:08 +0800

    feat(server): support dial using socks5, http, vmess, vless... (#92)

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,6 +1,26 @@
 package main
 
-import "os"
+import (
+	"os"
+
+	_ "github.com/daeuniverse/outbound/dialer/http"
+	_ "github.com/daeuniverse/outbound/dialer/juicity"
+	_ "github.com/daeuniverse/outbound/dialer/shadowsocks"
+	_ "github.com/daeuniverse/outbound/dialer/shadowsocksr"
+	_ "github.com/daeuniverse/outbound/dialer/socks"
+	_ "github.com/daeuniverse/outbound/dialer/trojan"
+	_ "github.com/daeuniverse/outbound/dialer/tuic"
+	_ "github.com/daeuniverse/outbound/dialer/v2ray"
+	_ "github.com/daeuniverse/outbound/transport/simpleobfs"
+	_ "github.com/daeuniverse/outbound/transport/tls"
+	_ "github.com/daeuniverse/outbound/transport/ws"
+	_ "github.com/daeuniverse/softwind/protocol/juicity"
+	_ "github.com/daeuniverse/softwind/protocol/shadowsocks"
+	_ "github.com/daeuniverse/softwind/protocol/trojanc"
+	_ "github.com/daeuniverse/softwind/protocol/tuic"
+	_ "github.com/daeuniverse/softwind/protocol/vless"
+	_ "github.com/daeuniverse/softwind/protocol/vmess"
+)
 
 func main() {
 	if err := Execute(); err != nil {
