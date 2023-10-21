67bd6048ff3fd2de73a78759b710ca6d4104873e
kev <yqlbu@bu.edu>
2023-10-21 23:12:21 +0800

    patch: reformat implementation

--- cmd/internal/shared/version.go
+++ cmd/internal/shared/version.go
@@ -3,16 +3,27 @@ package shared
 import (
 	"fmt"
 	"runtime"
+	"strings"
 
 	"github.com/juicity/juicity/config"
 )
 
+func multiline(parts ...string) string {
+	return strings.Join(parts, "\n")
+}
+
 func PrintVersion(cgoEnabled int) {
-	fmt.Printf("juicity-client version %v\n", config.Version)
-	fmt.Printf("go version %v %v/%v\n", runtime.Version(), runtime.GOOS, runtime.GOARCH)
-	fmt.Printf("CGO_ENABLED: %v\n", cgoEnabled)
+	fmt.Print(multiline(
+		fmt.Sprintf("juicity-client version %v", config.Version),
+		fmt.Sprintf("go version %v %v/%v", runtime.Version(), runtime.GOOS, runtime.GOARCH),
+		fmt.Sprintf("CGO_ENABLED: %v\n", cgoEnabled),
+	))
 }
 
 func GetVersion(cgoEnabled int) string {
-	return fmt.Sprintf("juicity-client version %v\ngo version %v %v/%v\nCGO_ENABLED: %v", config.Version, runtime.Version(), runtime.GOOS, runtime.GOARCH, cgoEnabled)
+	return multiline(
+		fmt.Sprintf("juicity-client version %v", config.Version),
+		fmt.Sprintf("go version %v %v/%v", runtime.Version(), runtime.GOOS, runtime.GOARCH),
+		fmt.Sprintf("CGO_ENABLED: %v", cgoEnabled),
+	)
 }
