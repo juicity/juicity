a27efa3d0f5e96b685cb9ca7d42b3e4b76794f2c
kev <31861128+yqlbu@users.noreply.github.com>
2023-11-15 19:21:03 +0800

    feat(cmd/version): add copy right and license info (#134)

--- cmd/internal/shared/version.go
+++ cmd/internal/shared/version.go
@@ -15,15 +15,19 @@ func multiline(parts ...string) string {
 func PrintVersion(cgoEnabled int) {
 	fmt.Print(multiline(
 		fmt.Sprintf("juicity-client version %v", config.Version),
-		fmt.Sprintf("go version %v %v/%v", runtime.Version(), runtime.GOOS, runtime.GOARCH),
+		fmt.Sprintf("go runtime %v %v/%v", runtime.Version(), runtime.GOOS, runtime.GOARCH),
 		fmt.Sprintf("CGO_ENABLED: %v\n", cgoEnabled),
+		"Copyright (c) 2023 juicity",
+		"License GNU AGPLv3 <https://github.com/juicity/juicity/blob/main/LICENSE>",
 	))
 }
 
 func GetVersion(cgoEnabled int) string {
 	return multiline(
 		fmt.Sprintf("juicity-client version %v", config.Version),
-		fmt.Sprintf("go version %v %v/%v", runtime.Version(), runtime.GOOS, runtime.GOARCH),
+		fmt.Sprintf("go runtime %v %v/%v", runtime.Version(), runtime.GOOS, runtime.GOARCH),
 		fmt.Sprintf("CGO_ENABLED: %v", cgoEnabled),
+		"Copyright (c) 2023 juicity",
+		"License GNU AGPLv3 <https://github.com/juicity/juicity/blob/main/LICENSE>",
 	)
 }
